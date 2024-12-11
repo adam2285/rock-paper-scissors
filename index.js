6c7631201b0c3082da340a5a82d84fb373425597
adam2285 <162269948+adam2285@users.noreply.github.com>
2024-12-11 16:05:39 +1100

    Finished Game (in console version). Finishing touches and CSS to be added.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /* Two players (computer and human) who each have to pick either RP or S.
 Choices then need to be compared when playing a round. 
 The winner recieves a point, the loser doesn't and in case of a tie no points.
-5 rounds will be plaued and at the end of the 5 rounds an overall winner is declared.
+5 rounds will be played and at the end of the 5 rounds an overall winner is declared.
 */
 
 // Counter variable
@@ -39,7 +39,7 @@ function playRound(){
 
 // Determine winner and update score
 function determineWinner(humanChoice, computerChoice){
-    if (humanChoice === computerChoice)
+    if (humanChoice === computerChoice){
         console.log(`You both selected ${humanChoice}. It's a draw`)  // Template literals used with ${ } to nest an expression within the string.
         roundsDrawn++
     } else if (
@@ -48,12 +48,40 @@ function determineWinner(humanChoice, computerChoice){
         (humanChoice === "rock" && computerChoice === "scissors")
     ){
         console.log(`You chose ${humanChoice} and the computer chose ${computerChoice}. You win!`)
-        humanScore++
+        humanScore++;
+        humanRoundsWon++;
+    } else {
+        console.log(`You chose ${humanChoice} and the computer chose ${computerChoice}. You lose!`)
+        computerScore++;
+        computerRoundsWon++;
     }
-}
 
-
-
-
+    console.log(`Your score is ${humanScore} and the computer score is ${computerScore}.`)
+ }
 
 // Play five rounds
+function playGame(){
+    for (let i = 0; i < 5; i++){               // 'for' expression creates a loop (if 'i' is less than '5', increment by '1')
+        console.log(`\nRound ${i+1}:`);        // '\n' creates a new line. Logs round number to the console.
+        playRound()
+    }
+
+    if (humanScore > computerScore) {
+        console.log('\nGame Summary');
+        console.log('You won!');
+        console.log(`After 5 rounds you won the game. You won ${humanRoundsWon} round(s), the computer won ${computerRoundsWon} round(s) and ${roundsDrawn} round(s) were a draw.`);
+        console.log('Thanks for playing!');
+    } else if (humanScore < computerScore) {
+        console.log('\nGame Summary');
+        console.log('You lost!');
+        console.log(`After 5 rounds you won the game. You won ${humanRoundsWon} round(s), the computer won ${computerRoundsWon} round(s) and ${roundsDrawn} round(s) were a draw.`);
+        console.log('Thanks for playing!');
+    } else {
+        console.log('\nGame Summary');
+        console.log(`After 5 rounds it was a Draw!. You won ${humanRoundsWon} round(s), the computer won ${computerRoundsWon} round(s) and ${roundsDrawn} round(s) were a draw.`);
+        console.log('Thanks for playing!');
+    }
+    
+}
+
+console.log(playGame())
